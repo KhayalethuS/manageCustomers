14eec1074b4cba863bec918a036adf5c14151326
Khayalethu Sofohlo <khayalethusofohlo@gmail.com>
2020-10-22 23:12:30 +0200

    fix(server): added body-parser and routes required

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+const bodyParser = require("body-parser");
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require("./routes/routes.js")(app);
 var port = process.env.PORT || 5000;
 
 app.listen(port);
-
 console.log(`Server running on: ${port}`);
