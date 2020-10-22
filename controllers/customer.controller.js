46a917d0af125f79ceb8720790d8e68ad6960acb
Khayalethu Sofohlo <khayalethusofohlo@gmail.com>
2020-10-22 23:10:44 +0200

    feat(customer controller): controller setup

---
+++ controllers/customer.controller.js
@@ -0,0 +1,39 @@
+const Customer = require("../model/customer.model");
+
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Body required!"
+    });
+  }
+
+  // Create a Customer
+  const customer = new Customer({
+    email: req.body.email,
+    name: req.body.name,
+    status: req.body.status
+  });
+
+  // Save Customer in the database
+  Customer.create(customer, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Something went wrong please try again, if error persist contact our support team."
+      });
+    else res.send(data);
+  });
+  
+};
+
+exports.index = (req, res) => {
+  Customer.index((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Something went wrong please try again, if error persist contact our support team."
+      });
+    else res.send(data);
+  });
+};
